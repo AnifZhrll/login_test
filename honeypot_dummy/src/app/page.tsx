43e9a0e5a9684e9d2e5f1dc04307fab73bef3b74
aneefdev <anamanif1@gmail.com>
2025-06-12 14:09:00 +0700

    add navbar & logout

--- honeypot_dummy/src/app/page.tsx
+++ honeypot_dummy/src/app/page.tsx
@@ -6,101 +6,99 @@ import { useState } from "react";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+
   const handleLogin = (e) => {
     e.preventDefault();
-  }
+  };
 
   const payload = {
     email: email,
-    password: password
-  }
+    password: password,
+  };
 
-  fetch("")
+  fetch("");
 
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm max-sm:mt-16">
+          <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
+            Login to your account
+          </h2>
+        </div>
 
-    return (
-        <>
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm max-sm:mt-16">
-        <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
-        Login to your account
-        </h2>
-      </div>
-
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border-2 border-slate-100 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6">
+            <div>
               <label
-                htmlFor="password"
+                htmlFor="email"
                 className="block text-sm font-medium text-gray-900"
               >
-                Password
+                Email address
               </label>
-              <div className="text-sm">
-                <Link
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </Link>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border-2 border-slate-100 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
+                />
               </div>
             </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                autoComplete="current-password"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border-2 border-slate-100 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
-              />
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <Link
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border-2 border-slate-100 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <Link
-              href="/home"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-3 text-md font-semibold text-white shadow-xs hover:bg-indigo-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Login
-            </Link>
-          </div>
+            <div>
+              <Link
+                href="/home"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-3 text-md font-semibold text-white shadow-xs hover:bg-indigo-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </Link>
+            </div>
 
-          <div>
-            <Link
-              href="/auth/signin"
-              className="w-full block p-3 bg-gray-200 text-gray-900 rounded-md text-center hover:bg-gray-300"
-            >
-              Login with Google
-            </Link>
-          </div>
-        </form>
+            <div>
+              <Link
+                href="/auth/signin"
+                className="w-full block p-3 bg-gray-200 text-gray-900 rounded-md text-center hover:bg-gray-300"
+              >
+                Login with Google
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-        </>
-        
-    )
+    </>
+  );
 }
 
 export default page;
