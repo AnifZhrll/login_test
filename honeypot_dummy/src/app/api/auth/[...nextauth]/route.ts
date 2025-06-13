a176e50f8ef0eaa3e29f6e3cb1eb0c8af9d39702
AnifZhrll <anamanif1@gmail.com>
2025-06-13 15:25:17 +0700

    clear console log

--- honeypot_dummy/src/app/api/auth/[...nextauth]/route.ts
+++ honeypot_dummy/src/app/api/auth/[...nextauth]/route.ts
@@ -12,12 +12,6 @@ export const authOptions: AuthOptions = {
     async jwt({ token, account }) {
       if (account) {
         try {
-        console.log("Google account object:", account);
-        console.log("id_token dari Google:", account.id_token);
-        console.log("access_token dari Google:", account.access_token);
-
-
-        console.log("Mengirim token ke backend:", account.id_token ?? account.access_token);
         
         // Kirim ID token dari Google ke Spring Boot
         const res = await fetch('http://localhost:8080/auth/verify', {
